48518828618bb9ac5068c530d0a331bf4ea1e911
D4rk_4ssi$T <108276894+dark-assist@users.noreply.github.com>
2024-04-14 18:11:02 +0530

    Add files via upload

--- metasploit-install.sh
+++ metasploit-install.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 clear
 echo "METASPLOIT INSTALLER FOR TERMUX BY SANATANI-HACKER"
-echo "Telegram:-https://t.me/temuxhacking"
-echo "Github:-https://github.com/dark-assist"
+echo "Telegram: https://t.me/temuxhacking"
+echo "Github: https://github.com/dark-assist"
 sleep 2.5
-echo "Updating termux...."
-apt update;yes|apt upgrade
+echo "Updating Termux..."
+apt update && apt upgrade -y
 clear
-echo "Installing linux distribution manager"
+echo "Installing Linux distribution manager..."
 apt install proot-distro -y
 clear
-echo "Installing Ubuntu for better MSF support"
+echo "Installing Ubuntu for better MSF support..."
 pd install ubuntu
-echo "updating Ubuntu"
-pd login ubuntu -- /bin/bash -c "apt update"
-pd login ubuntu -- /bin/bash -c "apt install curl && curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > /tmp/msfinstall"
-pd login ubuntu -- /bin/bash -c "chmod 755 /tmp/msfinstall"
-pd login ubuntu -- /bin/bash -c "cd /tmp && ./msfinstall"
-clear
-sleep 2
-echo "Creating link for MSF"
+echo "Updating Ubuntu and installing dependencies..."
+pd login ubuntu -- /bin/bash -c "apt update && apt install curl -y"
+echo "Installing Metasploit..."
+pd login ubuntu -- /bin/bash -c "curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > /tmp/msfinstall"
+pd login ubuntu -- /bin/bash -c "chmod 755 /tmp/msfinstall && cd /tmp && ./msfinstall"
+echo "Creating links for Metasploit commands..."
 echo "pd login ubuntu -- /bin/bash -c msfconsole" > $PREFIX/bin/msfconsole
 echo "pd login ubuntu -- /bin/bash -c msfvenom" > $PREFIX/bin/msfvenom
 echo "pd login ubuntu -- /bin/bash -c msfdb" > $PREFIX/bin/msfdb
@@ -28,6 +26,7 @@ echo "pd login ubuntu -- /bin/bash -c msfd" > $PREFIX/bin/msfd
 echo "pd login ubuntu -- /bin/bash -c msfrpc" > $PREFIX/bin/msfrpc
 chmod +x $PREFIX/bin/msf*
 clear
-echo "Thanks for using my repository"
+echo "Installation complete!"
+echo "Thank you for using my repository."
 sleep 2
-echo "You can start metasploit by runing msfconsole"
+echo "You can start Metasploit by running 'msfconsole'."
